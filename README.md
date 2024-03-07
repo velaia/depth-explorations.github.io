74664dfc019094bb271a76eca74b52811ffe79a8
Daniel N. Lang <daniel.lang@velaia.de>
2024-03-07 13:19:01 +0700

    Created start page, converted all images, and more

---
+++ README.md
@@ -0,0 +1,6 @@
+# TODO
+* [ ] Prepare so anyone can fork the repository or from a template with their own dependencies
+* [ ] write down dependencies
+* [ ] Make sure it runs in simple HTTP server before publishing
+
+# In Depth View of the World
--- prepare_images.py
+++ prepare_images.py
@@ -1,12 +1,28 @@
-import os, glob
+import os, glob, json
 from pathlib import Path
+import shutil
+
 
 
 depth_path = "/Users/daniel/Downloads/Camera Uploads.depth"
-img_path = "/Users/daniel/Downloads/Camera Uploads 2024\/03"
+img_path = "/Users/daniel/Downloads/Camera Uploads 2024_03"
 
-for file in glob.glob(depth_path + "/*"):
-    img_name = Path(file).name.replace("_img_depth", "")
+files = []
+
+for idx, file in enumerate(glob.glob(depth_path + "/*img_depth*")):
+    depth_name = Path(file).name
+    img_name = depth_name.replace("_img_depth", "").replace("png", "jpg")
     img = Path(img_path).joinpath(img_name)
-    print(img)
-    if img.is_file(): print(f"{img} is file")
+    if img.is_file():
+        print(f"{img} is file {idx}")
+        try:
+            shutil.copyfile(Path(img), Path('images/').joinpath(img_name))
+            print("File copied successfully.")
+        except shutil.Error as e:
+            print("Error copying file:", e)
+    files.append((depth_name, img_name))
+
+
+# write to a json file that can be read from JavaScript
+with open("image-map.json", "w+") as outfile:
+    outfile.write(json.dumps(files))

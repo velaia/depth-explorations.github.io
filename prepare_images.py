c34b5274522fb7f53ef1c08fad149bde02d4fc71
Daniel N. Lang <daniel.lang@velaia.de>
2024-03-07 14:00:49 +0700

    removed some images

--- prepare_images.py
+++ prepare_images.py
@@ -4,23 +4,24 @@ import shutil
 
 
 
-depth_path = "/Users/daniel/Downloads/Camera Uploads.depth"
-img_path = "/Users/daniel/Downloads/Camera Uploads 2024_03"
+depth_path = "images"
+img_path = "images"
 
 files = []
 
 for idx, file in enumerate(glob.glob(depth_path + "/*img_depth*")):
     depth_name = Path(file).name
-    img_name = depth_name.replace("_img_depth", "").replace("png", "jpg")
-    img = Path(img_path).joinpath(img_name)
-    if img.is_file():
-        print(f"{img} is file {idx}")
-        try:
-            shutil.copyfile(Path(img), Path('images/').joinpath(img_name))
-            print("File copied successfully.")
-        except shutil.Error as e:
-            print("Error copying file:", e)
-    files.append((depth_name, img_name))
+    img_name = depth_name.replace("_img_depth", "").replace("png", "webp")
+    new_depth_name = depth_name.replace("png", "webp")
+    # img = Path(img_path).joinpath(img_name)
+    # if img.is_file():
+        # print(f"{img} is file {idx}")
+        # try:
+        #     # shutil.copyfile(Path(img), Path('images/').joinpath(img_name))
+        #     print("File copied successfully.")
+        # except shutil.Error as e:
+        #     print("Error copying file:", e)
+    files.append((new_depth_name, img_name))
 
 
 # write to a json file that can be read from JavaScript

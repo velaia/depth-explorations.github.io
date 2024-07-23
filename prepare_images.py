8a2c8d5c22ec092994931d1fe6563f2aef60a968
Daniel N. Lang <daniel.lang@velaia.de>
2024-07-23 18:58:13 +0200

    cleaning Python helper script + some description

--- prepare_images.py
+++ prepare_images.py
@@ -1,28 +1,25 @@
-import os, glob, json
+"""Small helper tool to create a json file mapping images to their depth maps.
+
+Adjust the file paths and file extensions before each use."""
+
+import glob, json, random
 from pathlib import Path
-import shutil
 
 
 images_path = "images"
 depth_path = "v2/depth/"
 img_path = "v2/img/"
+# generate image map name with some randomness to not overwrite existing image map
+image_map_name = "image-map-" + str(random.randint(1_000_000, 9_999_999)) + ".json"
 
 files = []
 
 for idx, file in enumerate(glob.glob(images_path + "/" + depth_path + "/*.webp")):
     depth_name = Path(file).name
     img_name = depth_name.replace(".webp", ".jpg.webp")
-    # img = Path(img_path).joinpath(img_name)
-    # if img.is_file():
-        # print(f"{img} is file {idx}")
-        # try:
-        #     # shutil.copyfile(Path(img), Path('images/').joinpath(img_name))
-        #     print("File copied successfully.")
-        # except shutil.Error as e:
-        #     print("Error copying file:", e)
     files.append((depth_path + depth_name, img_path + img_name))
 
 
 # write to a json file that can be read from JavaScript
-with open("image-map-v2.json", "w+") as outfile:
+with open(image_map_name, "w+") as outfile:
     outfile.write(json.dumps(files))
